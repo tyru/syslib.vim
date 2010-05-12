91da033499fcbc6b51713f41cbed918021d1c5c7
tyru <tyru.exe@gmail.com>
2010-05-13 04:29:20 +0900

    Fix deserialize_args().

--- autoload/syslib.c
+++ autoload/syslib.c
@@ -10,6 +10,7 @@
 
 #if defined __linux__ || defined __CYGWIN__ || defined __APPLE__
 #   include <unistd.h>
+#   include <errno.h>
 #else
 #   error Your platform is not supported!!
 #endif
@@ -44,75 +45,127 @@ int create_hardlink(char *args);
 
 
 
-/* Argument types */
-typedef struct {
-    char *buf;
-    size_t buf_size;
-} Arg;
-
+/* NodeArg types */
+typedef struct NodeArg_tag {
+    struct NodeArg_tag *next;
+    char                 *buf;
+    size_t                buf_size;
+} NodeArg;
 
 
 /* Global variables */
-size_t cur_args_num = 0;
-Arg *the_args = NULL;
+static NodeArg *the_args = NULL;    // Top argument.
+static int last_errno = 0;
 
 
+
+static NodeArg*
+create_arg(const char *buf)
+{
+    NodeArg *node = malloc(sizeof(NodeArg));
+    if (node == NULL) {
+        abort();
+    }
+
+    node->next = NULL;
+    node->buf = strdup(buf);
+    node->buf_size = strlen(buf) + 1;
+
+    return node;
+}
 
 static void
-allocate_args(size_t args_num)
+set_buf_to_arg(NodeArg *arg, char *buf)
 {
-    if (the_args == NULL) {
-        the_args = calloc(sizeof(Arg), args_num);
-        if (the_args == NULL) {
+    size_t buf_size = strlen(buf) + 1;
+    assert(arg->buf != NULL);
+
+    if (arg->buf_size < buf_size) {
+        arg->buf = realloc(arg->buf, buf_size);
+        if (arg->buf == NULL) {
             abort();
         }
+        arg->buf_size = buf_size;
     }
-    else if (cur_args_num < args_num) {
-        realloc(the_args, sizeof(Arg) * args_num);
-        if (the_args == NULL) {
-            abort();
-        }
-        memset(the_args + sizeof(Arg), 0, args_num - cur_args_num);
-    } else {
-        // Shrink current `the_args`?
-    }
-    cur_args_num = args_num;
+    strcpy(arg->buf, buf);
 }
 
 
 
-static void
-allocate_arg_obj(Arg *arg, size_t buf_size)
+/* NodeArg functions */
+
+// TODO
+// static NodeArg*
+// serialize_args(NodeArg *args)
+// {
+// }
+
+static NodeArg*
+deserialize_args(const char *args)
 {
-    if (arg->buf == NULL) {
-        arg->buf = malloc(buf_size);
-        if (arg->buf == NULL) {
-            abort();
+    int debug_arg_count = 1;
+    NodeArg *prev_node_ptr = the_args;
+    char cur_arg[strlen(args)];    // c99
+    size_t cur_arg_pos = 0;
+    size_t pos = 0;
+    size_t len = strlen(args);
+
+    while (pos < len) {
+        switch (args[pos]) {
+        case '\xFE':
+            switch (args[pos + 1]) {
+            case '\0': /* No more bytes */
+                abort();
+                break;
+            case '\xFE':
+                cur_arg[cur_arg_pos++] = '\xFE';
+                pos += 2;
+                break;
+            case '\xFF':
+                cur_arg[cur_arg_pos++] = '\xFF';
+                pos += 2;
+                break;
+            default: /* Escaped but not special character */
+                abort();
+                break;
+            }
+            break;
+        case '\xFF':
+            cur_arg[cur_arg_pos] = '\0';
+
+            if (the_args == NULL) {
+                assert(prev_node_ptr == NULL);
+                the_args = prev_node_ptr = create_arg(cur_arg);
+            }
+            else if (prev_node_ptr->next == NULL) {
+                prev_node_ptr->next = create_arg(cur_arg);
+                prev_node_ptr = prev_node_ptr->next;
+            }
+            else {
+                set_buf_to_arg(prev_node_ptr->next, cur_arg);
+                prev_node_ptr = prev_node_ptr->next;
+            }
+
+            syslib_logf("argument %d: %s\n", debug_arg_count, cur_arg);
+            debug_arg_count++;
+
+            if (args[pos + 1] == '\xFF') {
+                return the_args;
+            }
+
+            cur_arg_pos = 0;
+            pos++;
+            break;
+        default:
+            cur_arg[cur_arg_pos++] = args[pos];
+            pos++;
+            break;
         }
     }
-    else {
-        realloc(arg->buf, buf_size);
-        if (arg->buf == NULL) {
-            abort();
-        }
-    }
-    arg->buf_size = buf_size;
+    abort();    /* End of bytes */
 }
 
 
-
-/* Argument functions */
-static Arg*
-deserialize_args(char *args)
-{
-    // TODO Count arguments' length.
-    size_t args_len;
-
-    // Set up `the_args`.
-    allocate_args(args_num);
-
-    // TODO Assign to `the_args`.
-    // Call `allocate_arg_obj()`.
 
     return the_args;
 }
